ce825f923d233f1882396d7daada2086367587ea
leybare <mdzyrd@gmail.com>
2023-04-02 18:56:58 +0800

    Search page UI modified.

--- lib/searchpage.dart
+++ lib/searchpage.dart
@@ -57,13 +57,12 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Find your dish',
+          'What would you like to eat?',
           style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.normal,
+            color: Colors.black,
           ),
         ),
+        backgroundColor: Colors.white,
       ),
       body: Container(
         padding: const EdgeInsets.only(
@@ -74,40 +73,56 @@ class _SearchPageState extends State<SearchPage> {
         ),
         child: Column(
           children: [
-            TextField(
-              textCapitalization: TextCapitalization.sentences,
-              onChanged: (value) {
-                setState(() {});
-              },
-              controller: _searchController,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(
-                  Icons.search,
+            Container(
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: _searchController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  prefixIcon: Icon(
+                    Icons.search,
+                  ),
+                  hintText: 'Search for food or cuisine',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
                 ),
-                hintText: "Type dish name",
               ),
             ),
             const SizedBox(
-              height: 10,
+              height: 15,
             ),
-            StreamBuilder<QuerySnapshot>(
-              stream: _dishes
-                  .where('name', isGreaterThanOrEqualTo: _searchController.text)
-                  .snapshots(), //connects to DB //build connection
-              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-                if (streamSnapshot.hasData) {
-                  return Expanded(
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _dishes
+                    .orderBy('name', descending: false)
+                    .where('name',
+                        isGreaterThanOrEqualTo: _searchController.text)
+                    .snapshots(), //connects to DB //build connection
+                builder:
+                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                  if (streamSnapshot.hasData) {
+                    return ListView.builder(
                       controller: _scrollController,
+                      shrinkWrap: true,
+                      //physics: const NeverScrollableScrollPhysics(),
                       itemCount: streamSnapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
+                      itemBuilder: (BuildContext context, int index) {
                         final DocumentSnapshot documentSnapshot =
                             streamSnapshot.data!.docs[index];
-
-                        return Card(
-                          margin: const EdgeInsets.all(10),
+                        return Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 10.0),
                           child: ListTile(
                             onTap: () {
                               Navigator.push(
@@ -119,54 +134,41 @@ class _SearchPageState extends State<SearchPage> {
                                 ),
                               );
                             },
-                            // ignore: prefer_interpolation_to_compose_strings
-                            title: Text(documentSnapshot['name']),
-                            subtitle: Text(documentSnapshot['category'] +
-                                ' / ' +
-                                documentSnapshot['cookTime'].toString() +
-                                ' minutes'),
-                            // trailing: SizedBox(
-                            //   width: 50,
-                            //   child: Row(
-                            //     // ignore: prefer_const_literals_to_create_immutables
-                            //     children: [
-                            //       ConstrainedBox(
-                            //         constraints: const BoxConstraints(
-                            //           minWidth: 40,
-                            //           minHeight: 44,
-                            //           maxWidth: 50,
-                            //           maxHeight: 64,
-                            //         ),
-                            //         child: Image.network(
-                            //           documentSnapshot['imgUrl'],
-                            //         ),
-                            //       ),
-                            //       // IconButton(
-                            //       //   color: Colors.greenAccent,
-                            //       //   icon: const Icon(Icons.remove_red_eye_rounded),
-                            //       //   onPressed: () => Navigator.push(
-                            //       //     context,
-                            //       //     MaterialPageRoute(
-                            //       //       builder: (context) => DetailPage(
-                            //       //         documentSnapshot: documentSnapshot,
-                            //       //       ),
-                            //       //     ),
-                            //       //   ),
-                            //       // ),
-                            //     ],
-                            //   ),
-                            // ),
+                            leading: Container(
+                              height: 80,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: DecorationImage(
+                                  image:
+                                      NetworkImage(documentSnapshot['imgUrl']),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              documentSnapshot['name'],
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            subtitle: Text(
+                              documentSnapshot['category'],
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
                           ),
                         );
                       },
-                    ),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                }
-
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+                },
+              ),
             ),
           ],
         ),
